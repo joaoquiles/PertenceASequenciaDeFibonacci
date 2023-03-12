2822d10d6c95d6e4069de436608b82695140c285
João Victor Santana Quiles <42985671+joaoquiles@users.noreply.github.com>
2023-03-12 16:38:14 -0300

    Add files via upload

---
+++ PertenceASequenciaDeFimonacci/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num, a = 0, b = 1, c = 0;
+        bool pertence = false;
+
+        Console.Write("Digite um número inteiro: ");
+        num = int.Parse(Console.ReadLine());
+
+        while (c < num)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+            if (c == num)
+            {
+                pertence = true;
+            }
+        }
+
+        if (pertence)
+        {
+            Console.WriteLine("{0} pertence à sequência de Fibonacci.", num);
+        }
+        else
+        {
+            Console.WriteLine("{0} não pertence à sequência de Fibonacci.", num);
+        }
+    }
+}
